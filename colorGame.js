eea111ef13b8d7d5e9ef4ef67bd9fce0f90462e3
nourebid <nour.ebid@gmail.com>
2020-10-28 05:09:56 +0200

    working on toggeling between 2 modes the hard and easy

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,46 @@
 let colors = generateRandomColors(6);
+let numSquares = 6;
 let squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let message = document.querySelector('#message');
 let h1 = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
+let easyBtn = document.querySelector('#easyBtn');
+let hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener('click', function(){
+    hardBtn.classList.remove('selected');
+    easyBtn.classList.add('selected');
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
+})
+
+hardBtn.addEventListener('click', function(){
+    hardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+        squares[i].style.display = 'block';
+    }
+})
 
 resetButton.addEventListener('click', function(){
     //generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     //pick a new random color from array
     pickedColor = pickColor();
     //change the colorDisplay to match the picked
